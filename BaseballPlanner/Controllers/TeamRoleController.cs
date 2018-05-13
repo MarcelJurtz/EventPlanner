9704127b8f3be695bf3791a7dfe03402046e8213
MarcelJurtz <jurtzmarcel@gmail.com>
2018-05-13 20:11:16 +0200

    Add header, sidebar and temporary fix bootstrap link

---
+++ BaseballPlanner/Controllers/TeamRoleController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Planner.Models.Repository;
+
+namespace BaseballPlanner.Controllers
+{
+    public class TeamRoleController : Controller
+    {
+        private readonly ITeamRoleRepository _teamRoleRepository;
+
+        public TeamRoleController(ITeamRoleRepository teamRoleRepository)
+        {
+            _teamRoleRepository = teamRoleRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_teamRoleRepository.TeamRoles);
+        }
+    }
+}
