1d6d6e320b94fd44c036c291b4a2658864c035e4
MarcelJurtz <jurtzmarcel@gmail.com>
2018-05-20 14:49:48 +0200

    Add migration for fixed model

---
+++ BaseballPlanner/Migrations/20180520124712_EventModelUpdate.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Planner.Migrations
+{
+    public partial class EventModelUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CoachesRequired",
+                table: "Events",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PlayersRequired",
+                table: "Events",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ScorersRequired",
+                table: "Events",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SeatsRequired",
+                table: "Events",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UmpiresRequired",
+                table: "Events",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CoachesRequired",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "PlayersRequired",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "ScorersRequired",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "SeatsRequired",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "UmpiresRequired",
+                table: "Events");
+        }
+    }
+}
