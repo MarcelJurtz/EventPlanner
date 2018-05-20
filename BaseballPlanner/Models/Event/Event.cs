1aff3f27b4d4279669035abc61241d315e6a5d35
MarcelJurtz <jurtzmarcel@gmail.com>
2018-05-20 13:16:59 +0200

    Update event model display names

--- BaseballPlanner/Models/Event/Event.cs
+++ BaseballPlanner/Models/Event/Event.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
@@ -16,18 +15,26 @@ namespace Planner.Models
         [StringLength(100)]
         public string Designation { get; set; }
 
+        [DisplayName("Beschreibung")]
         [StringLength(500)]
         public string Description { get; set; }
 
+        [DisplayName("Ort")]
         [StringLength(50)]
         public string Location { get; set; }
 
+        [DisplayName("Start")]
         [Required(ErrorMessage = "Bitte geben Sie einen Startzeitpunkt an")]
         public DateTime Start { get; set; }
+
+        [DisplayName("Ende")]
         public DateTime? End { get; set; }
 
+        [DisplayName("Treffpunkt")]
         [StringLength(100)]
         public string MeetingLocation { get; set; }
+
+        [DisplayName("Treffpunkt Uhrzeit")]
         public DateTime MeetingTime { get; set; }
 
         public DateTime Created { get; set; }
