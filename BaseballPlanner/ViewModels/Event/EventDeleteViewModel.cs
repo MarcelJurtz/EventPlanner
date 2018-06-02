72a25b9df44ffec78e296e5cfa3f11cd47df997f
MarcelJurtz <jurtzmarcel@gmail.com>
2018-06-02 12:47:28 +0200

    Add event deletion option

---
+++ BaseballPlanner/ViewModels/Event/EventDeleteViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Planner.ViewModels
+{
+    public class EventDeleteViewModel
+    {
+        public Models.Event SelectedEvent { get; set; }
+        public int ParticipatingUsers { get; set; }
+    }
+}
